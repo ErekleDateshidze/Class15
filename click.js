84973e6ab2d227856c6baeb146071d262cc8b51b
Erekle99 <erekledateshidze99@gmail.com>
2022-06-10 16:25:59 +0400

    1

--- click.js
+++ click.js
@@ -1,3 +1,25 @@
-function mtFunction() {
-    document.getElementById; ("demo").innerHTML = "hidden text"
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "hidden text"
+// }
+
+
+// document.getElementById("click").onclick = function () { myFunction() };
+
+
+
+// function toogleFunction() {
+//     var element = document.getElementById("myDiv");
+//     element.classList.toggle("mystyle")
+// }  
+
+
+
+var button = document.getElementById("clickme");
+count = 0;
+button.onclick = function () {
+    count += 1;
+    button.innerHTML = "click me :" + count;
+    if (count == 10) {
+        count -= 1;
+    }
 }
